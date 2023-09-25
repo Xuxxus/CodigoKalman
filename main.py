b15501dbc103f840829199deecc1ba5707e2c38a
Gabriel Agune <79295632+GabrielAgune@users.noreply.github.com>
2023-09-25 19:09:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import numpy as np
+from numpy import dot, ndarray
+import numpy.linalg as linalg
 import math
-from filterpy.kalman import KalmanFilter, predict
+from filterpy.kalman import predict, update
+
 
 n_roll_real = 3
 n_pitch_real = 3
@@ -24,6 +27,26 @@ acelCalibrationYaw = [1.07, 0.97, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0
 # Criar for com loop para pegar a cada 7 dados fornecidos pelo cartão SD (Traduzir do código C#)
 # Multiplicação de matrizes feita, agora ver transformação para quaternion e depois manipulação das strings
 
+c = np.array([[1.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+              [0.0, 1.0, 0.0, 0.0, 0.0, 0.0]])
+
+r = np.array([[n_roll_real, 0.0],
+              [0.0, n_pitch_real]])
+
+x = np.array([[0.0],
+              [0.0],
+              [0.0],
+              [0.0],
+              [0.0],
+              [0.0]])
+
+u = np.array([[1.0],
+              [2.0],
+              [3.0]])
+
+a_shape = (6, 6)
+P = np.zeros(a_shape)
+
 with open('Teste.txt') as f:
     contents = f.read()
     dados = contents.split(',')
@@ -31,6 +54,34 @@ with open('Teste.txt') as f:
 
 for i in range(0, len(dados), 7):
     Ti = float(dados[i + 6])
+
+    q = np.array([[m_roll_real * Ti * Ti, 0.0, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, m_pitch_real * Ti * Ti, 0.0, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, m_yaw_real * Ti * Ti, 0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0, m_roll_real, 0.0, 0.0],
+                  [0.0, 0.0, 0.0, 0.0, m_pitch_real, 0.0],
+                  [0.0, 0.0, 0.0, 0.0, 0.0, m_yaw_real]])
+
+    b = np.array([[Ti, 0.0, 0.0],
+                  [0.0, Ti, 0.0],
+                  [0.0, 0.0, Ti],
+                  [0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0]])
+
+    a = np.array([[1.0, 0.0, 0.0, -Ti, 0.0, 0.0],
+                  [0.0, 1.0, 0.0, 0.0, -Ti, 0.0],
+                  [0.0, 0.0, 1.0, 0.0, 0.0, -Ti],
+                  [0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
+                  [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
+                  [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+
+    #nstate_nobs = (6, 2)
+    #nobs_nobs = (2, 2)
+
+    #S = np.zeros(nobs_nobs)
+    #k = np.zeros(nstate_nobs)
+
     # Recebimento -> ACCX, ACCY, ACCZ, GYRX, GYRY, GYRXZ e Tempo
     # dados[i], dados[i+1], dados[i+2], dados[i+3], dados[i+4], dados[i+5], dados[i+6]
 
@@ -54,56 +105,36 @@ for i in range(0, len(dados), 7):
     ratePitch = (float(dados[i + 4]) / 131) - rateCalibrationPitch[count]
     rateYaw = (float(dados[i + 5]) / 131) - rateCalibrationYaw[count]
 
-    a = np.array([[1.0, 0.0, 0.0, -Ti, 0.0, 0.0],
-         [0.0, 1.0, 0.0, 0.0, -Ti, 0.0],
-         [0.0, 0.0, 1.0, 0.0, 0.0, -Ti],
-         [0.0, 0.0, 0.0, 1.0, 0.0, 0.0],
-         [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
-         [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
+    # y = np.matmul(a, x)
+    # z = np.matmul(b, u)
+    # p = y + z
 
-    x = np.array([[0.0],
-         [0.0],
-         [0.0],
-         [0.0],
-         [0.0],
-         [0.0]])
-
-    b = np.array([[Ti, 0.0, 0.0],
-         [0.0, Ti, 0.0],
-         [0.0, 0.0, Ti],
-         [0.0, 0.0, 0.0],
-         [0.0, 0.0, 0.0],
-         [0.0, 0.0, 0.0]])
-
-    u = np.array([[1.0],
-         [2.0],
-         [3.0]])
-
-    q = np.array([[m_roll_real * Ti * Ti, 0.0, 0.0, 0.0, 0.0, 0.0],
-         [0.0, m_pitch_real * Ti * Ti, 0.0, 0.0, 0.0, 0.0],
-         [0.0, 0.0, m_yaw_real * Ti * Ti, 0.0, 0.0, 0.0],
-         [0.0, 0.0, 0.0, m_roll_real, 0.0, 0.0],
-         [0.0, 0.0, 0.0, 0.0, m_pitch_real, 0.0],
-         [0.0, 0.0, 0.0, 0.0, 0.0, m_yaw_real]])
-
-    a_shape = (6, 6)
-    P = np.zeros(a_shape)
-
-    #y = np.matmul(a, x)
-    #z = np.matmul(b, u)
-    #p = y + z
-
-    #print(np.array(y))
-    #print(np.array(z))
+    # print(np.array(y))
+    # print(np.array(z))
     x, P = predict(x, P, a, q, u, b)
-
+    print("----------------Predict-----------------")
     print('x =', x)
     print('P =', P)
-    #print("Array %d" % (count + 1))
-    #print(np.array(p))
-    #x_barra[count] = np.array(p)
+
+    S = dot(dot(c, P), c.T) + r
+
+    is_nosingular = bool(S.inv(S))
+
+    k = dot(dot(P, c.T), S)
+
+    if is_nosingular:
+        x, P = update(x, P, 1, r, c)
+        print("----------------Update-----------------")
+        print('x =', x)
+        print('P =', P)
+
+
+
+    # print("Array %d" % (count + 1))
+    # print(np.array(p))
+    # x_barra[count] = np.array(p)
 
     count += 1
 
-#print("Array xbarra: ")
-#print(x_barra[0])
+# print("Array xbarra: ")
+# print(x_barra[0])
