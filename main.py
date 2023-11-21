87367ffe5667f51ddc0f1bbaed4629abf9b81842
Gabriel Agune <79295632+GabrielAgune@users.noreply.github.com>
2023-11-21 11:52:53 -0300

    Update main.py
    Main funcionando sem o yaw

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import numpy as np
 from numpy import dot
 import math
 from filterpy.kalman import predict, update
+import matplotlib.pyplot as plt
 
 float_formartter = "{:.6f}".format
 np.set_printoptions(formatter={'float_kind':float_formartter})
+
+xbarra = []
 
 
 def get_quaternion_from_euler(roll, pitch, yaw):
@@ -24,7 +27,7 @@ def get_quaternion_from_euler(roll, pitch, yaw):
   qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
   qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
 
-  return [qx, qy, qz, qw]
+  return [qw, qx, qy, qz]
 
 def getCofactor(mat, temp, p, q, n):
     i = 0
@@ -97,26 +100,27 @@ m_roll_real = 0.1
 m_pitch_real = 0.1
 m_yaw_real = 0.1
 i = 0
-count = 0
 x_barra = []
 tempo = []
-'''
+
 rateCalibrationRoll = [-0.13, -2.85, 1.69, -4.2, 7.55, -3.95, 2.65, -26.7, -1.0, -3.65, 0.0, 0.0, 0.0]
 rateCalibrationPitch = [0.83, 1.68, -0.89, 2.45, 1.07, -2.74, -3.26, -1.29, 0.72, -0.31, 0.0, 0.0, 0.0]
-rateCalibrationYaw = [0.6, 0.53, 0.24, -1.15, 1.87, -0.34, -0.59, 0.65, 0.62, 0.45, 0.0, 0.0, 0.0]'''
-
-rateCalibrationRoll = [-3.95, -1.0, 1.69, -4.2, 7.55, -3.95, 2.65, -26.7, -1.0, -3.65, 0.0, 0.0, 0.0]
-rateCalibrationPitch = [-2.74, 0.72, -0.89, 2.45, 1.07, -2.74, -3.26, -1.29, 0.72, -0.31, 0.0, 0.0, 0.0]
-rateCalibrationYaw = [-0.34, 0.62, 0.24, -1.15, 1.87, -0.34, -0.59, 0.65, 0.62, 0.45, 0.0, 0.0, 0.0]
+rateCalibrationYaw = [0.6, 0.53, 0.24, -1.15, 1.87, -0.34, -0.59, 0.65, 0.62, 0.45, 0.0, 0.0, 0.0]
 
 '''
+rateCalibrationRoll = [-3.95, -1.0, 1.69, -4.2, 7.55, -3.95, 2.65, -26.7, -1.0, -3.65, 0.0, 0.0, 0.0]
+rateCalibrationPitch = [-2.74, 0.72, -0.89, 2.45, 1.07, -2.74, -3.26, -1.29, 0.72, -0.31, 0.0, 0.0, 0.0]
+rateCalibrationYaw = [-0.34, 0.62, 0.24, -1.15, 1.87, -0.34, -0.59, 0.65, 0.62, 0.45, 0.0, 0.0, 0.0]'''
+
+
 acelCalibrationRoll = [1.04, 1.03, 1.04, 1.03, 1.0, 1.03, 1.06, 1.04, 1.03, 0.96, 0.0, 0.0, 0.0]
 acelCalibrationPitch = [0.98, 0.98, 0.99, 1.0, 0.99, 1.0, 0.99, 0.98, 0.99, 1.01, 0.0, 0.0, 0.0]
-acelCalibrationYaw = [0.95, 0.94, 0.94, 0.95, 0.93, 0.99, 0.74, 1.0, 0.95, 0.89, 0.0, 0.0, 0.0]'''
+acelCalibrationYaw = [0.95, 0.94, 0.94, 0.95, 0.93, 0.99, 0.74, 1.0, 0.95, 0.89, 0.0, 0.0, 0.0]
 
+'''
 acelCalibrationRoll = [1.03, 1.03, 1.04, 1.03, 1.0, 1.03, 1.06, 1.04, 1.03, 0.96, 0.0, 0.0, 0.0]
 acelCalibrationPitch = [1.0, 0.99, 0.99, 1.0, 0.99, 1.0, 0.99, 0.98, 0.99, 1.01, 0.0, 0.0, 0.0]
-acelCalibrationYaw = [0.99, 0.95, 0.94, 0.95, 0.93, 0.99, 0.74, 1.0, 0.95, 0.89, 0.0, 0.0, 0.0]
+acelCalibrationYaw = [0.99, 0.95, 0.94, 0.95, 0.93, 0.99, 0.74, 1.0, 0.95, 0.89, 0.0, 0.0, 0.0]'''
 
 # dados = [1,2,3,4,5,6,7,8,9,2,1,2,3,4]
 # Impelmentar busca de arquivos e leitura (pandas)
@@ -151,7 +155,7 @@ S = np.zeros(nobs_nobs)
 k = np.zeros(nstate_nobs)
 z = np.zeros(n_z)
 
-with open('Teste.txt') as f:
+with open('/content/drive/MyDrive/TCC/Kalman/take1.txt') as f:
     contents = f.read()
     dados = contents.split(',')
     print(dados)
@@ -161,10 +165,17 @@ n_sensor = int(input("Insira o número de sensores: "))
 nome_sensores = []
 for i in range(n_sensor):
     nome_sensores.append(str(input("Nome do sensor %d: " %(i+1))))
+    xbarra.append([])
 
-
+print(len(dados))
+count = 0
 for i in range(0, len(dados), 7):
-    Ti = float(dados[i + 6])
+
+    TT = float(dados[i+6])
+    if i < 7*n_sensor:
+      Ti = float(dados[i+6])
+    if i > 7*n_sensor:
+      Ti =  (float(dados[i+6]) - float(dados[i+6-(7*n_sensor)]))
 
     q = np.array([[m_roll_real * Ti * Ti, 0.0, 0.0, 0.0, 0.0, 0.0],
                   [0.0, m_pitch_real * Ti * Ti, 0.0, 0.0, 0.0, 0.0],
@@ -187,27 +198,41 @@ for i in range(0, len(dados), 7):
                   [0.0, 0.0, 0.0, 0.0, 1.0, 0.0],
                   [0.0, 0.0, 0.0, 0.0, 0.0, 1.0]])
 
-
+    dados[i] = (float(dados[i ]) - (acelCalibrationRoll[count] -1)) #altereiiiiiiiii
+    dados[i + 1] = (float(dados[i + 1]) - (acelCalibrationPitch[count] -1))  #altereiiiiiiiii
+    dados[i + 2] =  (float(dados[i + 2]) - (acelCalibrationYaw[count] -1)) #altereiiiiiiiii
 
     # Recebimento -> ACCX, ACCY, ACCZ, GYRX, GYRY, GYRXZ e Tempo
     # dados[i], dados[i+1], dados[i+2], dados[i+3], dados[i+4], dados[i+5], dados[i+6]
 
     # AngleRoll = atan(AccY / sqrt(AccX * AccX + AccZ * AccZ)) * 1 / (3.14159265 / 180);
-    angleRoll = math.atan((float(dados[i + 1]) - acelCalibrationPitch[count]) / math.sqrt(
+    angleRoll = math.atan((float(dados[i + 1]) ) / math.sqrt(
+            (float(dados[i])) * (float(dados[i]) ) + (
+                    (float(dados[i + 2]) ) * (
+                    float(dados[i + 2]) ))))
+
+
+    '''math.atan((float(dados[i + 1]) - acelCalibrationPitch[count]) / math.sqrt(
         ((float(dados[i]) - acelCalibrationRoll[count]) * (float(dados[i]) - acelCalibrationRoll[count])) + (
                 (float(dados[i + 2]) - acelCalibrationYaw[count]) * (
-                float(dados[i + 2]) - acelCalibrationYaw[count])))) * (1 / (math.pi / 180))
+                float(dados[i + 2]) - acelCalibrationYaw[count]))))'''
 
     # AnglePitch = -atan(AccX/sqrt(AccY*AccY + AccZ*AccZ))*1/(3.14159265/180);
-    anglePitch = -math.atan((float(dados[i]) - acelCalibrationRoll[count]) / math.sqrt(
+    anglePitch = -math.atan((float(dados[i]) ) / math.sqrt(      #altereiiiiiiiii
+            ((float(dados[i + 1]) ) * (float(dados[i + 1]) )) + (
+                    (float(dados[i + 2]) ) * (
+                    float(dados[i + 2]) ))))
+
+    '''-math.atan((float(dados[i]) - acelCalibrationRoll[count]) / math.sqrt(
         ((float(dados[i + 1]) - acelCalibrationPitch[count]) * (float(dados[i + 1]) - acelCalibrationPitch[count])) + (
                 (float(dados[i + 2]) - acelCalibrationYaw[count]) * (
-                float(dados[i + 2]) - acelCalibrationYaw[count])))) * (1 / (math.pi / 180))
+                float(dados[i + 2]) - acelCalibrationYaw[count])))) #* (1 / (math.pi / 180)'''
 
-    #angleYaw =
+    #angleYaw = (math.atan(((float(dados[i + 2]))/math.sqrt((float(dados[i]))*(float(dados[i]))+(float(dados[i+1]))*(float(dados[i+1]))))))
 
     z[0] = angleRoll
     z[1] = anglePitch
+    #z[2] = angleYaw
 
     # rateRoll = (Convert.ToDouble(list[i + 3]) / 131) - rateCalibrationRoll[count];
     # ratePitch = (Convert.ToDouble(list[i + 4]) / 131) - rateCalibrationPitch[count];
@@ -221,6 +246,10 @@ for i in range(0, len(dados), 7):
     u[1] = ratePitch
     u[2] = rateYaw
 
+    if (i//7)%(n_sensor) == 0 and i != 0:
+
+        tempo.append(TT)
+        count = 0
     # y = np.matmul(a, x)
     # z = np.matmul(b, u)
     # p = y + z
@@ -243,30 +272,37 @@ for i in range(0, len(dados), 7):
         #print("----------------Update-----------------")
         #print('x =', x)
         #print('P =', P)
+        xbarra[count].append(get_quaternion_from_euler(x[2][0],x[1][0],x[0][0]))
+        #xbarra[count].append(x_barra)
+        #x_barra = []
 
-    x_barra.append(get_quaternion_from_euler(x[0][0],x[1][0],x[2][0]))
 
     count += 1
-    if (i+1)%n_sensor == 0:
-        tempo.append(Ti)
-        count = 0
-
+    print(count)
     if i == 0:
+        tempo.append(TT)
         print("Ângulo de euler inicial: " + str(x[0][0]) + str(x[1][0]) + str(x[2][0]))
     # print("Array %d" % (count + 1))
     # print(np.array(p))
     #x_barra[count] = np.array(p)
 
-
 #print("Xbarra: ",x_barra)
 # print("Array xbarra: ")
 # print(x_barra[0])
-#print(len(tempo))
-print(len(x_barra))
+print(len(tempo))
+print('----')
+print(len(xbarra[0]))
+print(xbarra)
+#print(x_barra)
 
-for i in range(len(x_barra)):
-    for j in range(4):
-        x_barra[i][j] = "%.16f" % x_barra[i][j]
+'''
+for k in range(2):
+    for i in range(len(xbarra[0])-1):
+        for j in range(4):
+            xbarra[k][i][j] = "%.16f" % xbarra[k][i][j]
+'''
+
+print(xbarra)
 
 #time.sleep(10)
 
@@ -278,8 +314,7 @@ for l in range(len(nome_sensores)):
 f1.write("\n")
 f1.write(str(tempo[0]))
 for n in range(n_sensor):
-    f1.write("\t" + str(x_barra[n][0]) + "," + str(x_barra[n][1]) + "," + str(x_barra[n][2]) + "," + str(x_barra[n][3]))
-
+    f1.write("\t" + str(xbarra[n][0][0]) + "," + str(xbarra[n][0][1]) + "," + str(xbarra[n][0][2]) + "," + str(xbarra[n][0][3]))
 
 f1 = open('opensimTeste.sto', 'w')
 f1.write("DataRate=100.000000\nDataType=Quaternion\nversion=3\nOpenSimVersion=4.4\nendheader\n")
@@ -287,10 +322,10 @@ f1.write('time')
 for l in range(len(nome_sensores)):
     f1.write('\t' + nome_sensores[l])
 f1.write("\n")
-for k in range(0, len(x_barra), 3):
-    f1.write(str(tempo[k // n_sensor]))
-    for j in range(n_sensor):
-        f1.write("\t" + str(x_barra[k + j][0]) + "," + str(x_barra[k + j][1]) + "," + str(x_barra[k + j][2]) + "," + str(x_barra[k + j][3]))
+for k in range(0, len(xbarra[0])-1, 1):
+    f1.write(str(tempo[k]))
+    for j in range(0, n_sensor, 1):
+        f1.write("\t" + str(xbarra[j][k][0]) + "," + str(xbarra[j][k][1]) + "," + str(xbarra[j][k][2]) + "," + str(xbarra[j][k][3]))
              # "  " + str(x_barra[k + 1][0]) + "," + str(x_barra[k + 1][1]) + "," + str(x_barra[k + 1][2]) + "," + str(x_barra[k + 1][3]) + "    " + str(x_barra[k + 2][0]) + "," + str(x_barra[k + 2][1]) + "," + str(x_barra[k + 2][2]) + "," + str(x_barra[k + 2][3]) + "\n")
     f1.write("\n")
 
